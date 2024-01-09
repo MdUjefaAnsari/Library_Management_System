f903b3cbc2faeae32a88f785a1c4712f5c90fc03
MdUjefaAnsari <mdujefaansari@gmail.com>
2024-01-09 09:31:18 +0530

    added public

--- index.js
+++ index.js
@@ -39,13 +39,7 @@ app.use(expressLayouts);
 app.set('layout','./layouts/main');
 app.set('view engine','ejs');
 
-// Set 'views' directory for any views 
-// being rendered res.render()
-app.set('views', path.join(__dirname, 'views'));
-
-// Set view engine as EJS
-app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //flash messgage
